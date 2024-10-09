fe35a2b74b2368c132952d6e25ad32b4cf207a47
Andrés Salgado <andressalgado41@gmail.com>
2024-10-09 11:54:56 -0500

    Remove code variables, array and loops. Rename Main.kt to Clases.kt

--- src/Clases.kt
+++ src/Clases.kt
@@ -1,7 +1,4 @@
 fun main() {
-    //tiposDatos()
-    //arrays()
-   // calcularFactorial(4)
 
     val perro1 = Perro("Rufian","Pequeño", 9)
     val perro2 = Perro("Maximo","Mediano", -45)
@@ -46,40 +43,6 @@ class Perro (var nombre:String, var estatura:String, var edad:Int) {
     }
 }
 
-fun tiposDatos(){
-    var nombre:String = "Andrés"            // variable string poniendo explicitamente el tipo de dato
-    val apellido = "Salgado"                // constante string si poner tipo de dato, kotlin infiere el tipo
-    var edad:Short = 30                     // variable entero (short)
-    var esMayorEdad = true                  // variable boolean
-
-    print("El nombre de la persona es: " + nombre)              // imprimir en consola sin salto de línea
-    println(" su apellido es: $apellido")                       // imprimir en consola con salto de línea
-    println("Es mayor de edad: " + esMayorEdad)                 // Concatenación con +
-    println("Tiene la edad de: $edad")                          // Concatenacion con template
-}
-
-fun arrays () {
-    var arregloNumeros:Array<Int> = arrayOf(1,2,3,4,5)          // Array de enteros, arrayOf método que permite inicializar el array con valores establecildos
-    var nombreMeses = arrayOf("Enero","Febrero")                // Array de Strings (cadena de texto) infiriendo el tipo
-    val mi_matriz = Array(5) {0}                           // Usando la Clase Array que permite establece el tamaño o posiciones del array (5), y {0} inicializa el array en 0 en todas la posiciones
-
-
-    println(nombreMeses[1])         // imprime el elemento 2 de la posición 1
-
-}
-
-// loop o bucles
-fun calcularFactorial(numero: Int): Long {
-    var resultado = 1L
-    for (i in 2..numero) {
-        resultado *= i
-        println("$i ---> $resultado")
-    }
-    println("El factorial de $numero es $resultado")
-    return resultado
-}
-
-// Funciones, son bloques de código que permite resolver un problema
 /*
 Modificadores de acceso: nos permite definir el nivel de acceso es decir, como se va a poder acceder a la función desde
 fuera de la clase donde se a definido.
